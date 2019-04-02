8f3fc45c68db716fff873f78eddd39befd680623
Hao LI <imleehao@gmail.com>
2019-04-02 22:11:52 +0800

    Update DefineStack.py

--- 剑指offer/DefineStack.py
+++ 剑指offer/DefineStack.py
@@ -3,6 +3,9 @@
 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
 '''
 
+# emmmm...maybe time complexity is not correct...
+# later, I will check it again.
+
 class Solution:
     def __init__(self):
         self.res = []
