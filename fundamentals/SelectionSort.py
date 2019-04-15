9e20722c0c81e08fac381b111e309045e707edb7
lance-lh <imleehao@gmail.com>
2019-04-15 20:41:37 +0800

    selection sort

---
+++ fundamentals/SelectionSort.py
@@ -0,0 +1,26 @@
+# https://github.com/hustcc/JS-Sorting-Algorithm/blob/master/2.selectionSort.md
+
+def selectionsort(arr):
+    '''
+    :param arr: list
+    :return: list
+    '''
+    for i in range(len(arr) - 1):
+        # 记录最小数的索引
+        # 每次先把外循环索引作为当轮最小索引
+        minIndex = i
+        # 找到最小索引
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        # i 不是最小数时，将 i 和最小数进行交换
+        # 注意不仅仅是将最小数赋给外循环索引位置，
+        # 同时最小索引需要和外循环索引对应元素交换，以继续后续排序
+        if i != minIndex:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
+
+# test
+a = [5, 2, 4, 3, 6, 8, 9, 7]
+print(a)
+print(selectionsort(a))
