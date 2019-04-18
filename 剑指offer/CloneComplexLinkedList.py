637791998807a74c96cd16aa0ed76b456b76c3f7
haolee <imleehao@gmail.com>
2019-04-18 21:52:25 +0800

    Clone Complex Linked List

---
+++ 剑指offer/CloneComplexLinkedList.py
@@ -0,0 +1,25 @@
+'''
+输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，
+另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
+（注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+'''
+
+# 递归处理
+# next为特殊节点
+# random为普通节点
+
+# -*- coding:utf-8 -*-
+# class RandomListNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.next = None
+#         self.random = None
+class Solution:
+    # 返回 RandomListNode
+    def Clone(self, pHead):
+        if not pHead:
+            return None
+        newnode = RandomListNode(pHead.label)
+        newnode.random = pHead.random
+        newnode.next = self.Clone(pHead.next)
+        return newnode
