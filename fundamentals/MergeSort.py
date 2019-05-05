dd82fbc1f389c4f13011c995481b8b351ee8ab1f
Hao LI <imleehao@gmail.com>
2019-05-05 14:12:06 +0800

    update merge sort

--- fundamentals/MergeSort.py
+++ fundamentals/MergeSort.py
@@ -1,4 +1,5 @@
 # https://github.com/hustcc/JS-Sorting-Algorithm/blob/master/5.mergeSort.md
+# https://www.cnblogs.com/chengxiao/p/6194356.html
 
 # complexity: O(nlogn)
 def mergeSort(arr):
@@ -11,16 +12,23 @@ def mergeSort(arr):
     middle = len(arr)//2
     left, right = arr[0:middle], arr[middle:]
     return merge(mergeSort(left), mergeSort(right))
+    # after this, divide arr into each seperate num and it looks like a binary tree with log n depth.
 
+# then conquer, it needs n steps
+# so complexity is O(nlogn)
 def merge(left,right):
+    # space for time
     result = []
+    # compare left and right elements, add it to the res
     while left and right:
         if left[0] <= right[0]:
             result.append(left.pop(0))
         else:
             result.append(right.pop(0))
+    # if right branch is empty, add the left elements into the end of res
     while left:
         result.append(left.pop(0))
+    # if left branch is empty, add the right elements into the end of res
     while right:
         result.append(right.pop(0))
     return result
