f876aaf30cfbac0ad63cb05bf4ca24e4a1467ba9
haolee <imleehao@gmail.com>
2019-06-02 11:05:23 +0800

    update FindNumsAppearOnce

--- 剑指offer/FindNumsAppearOnce.py
+++ 剑指offer/FindNumsAppearOnce.py
@@ -5,7 +5,8 @@
 '''
 
 
-
+# hash table
+'''
 class Solution:
     # 返回[a,b] 其中ab是出现一次的两个数字
     def FindNumsAppearOnce(self, array):
@@ -17,6 +18,36 @@ class Solution:
             if d[i] == 1:
                 res.append(i)
         return res
+'''
+
+# bit manipulation
+class Solution:
+    # 返回[a,b] 其中ab是出现一次的两个数字
+    def FindNumsAppearOnce(self, array):
+        if not array:
+            return []
+        # 异或
+        tmp = 0
+        for i in array:
+            tmp ^= i
+        # 找最低位1出现的index
+        # ind from right to left
+        ind = 0
+        while tmp & 1 == 0:
+            tmp >>= 1
+            ind += 1
+        # 分组异或
+        a = b = 0
+        for i in array:
+            if self.isBit(i, ind):
+                a ^= i
+            else:
+                b ^= i
+        return [a, b]
+
+    def isBit(self, num, ind):
+        num = num >> ind
+        return num & 1
 
 # test
 nums = [1,1,4,5,6,7,4,7]
