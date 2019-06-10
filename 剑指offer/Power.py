20e5b4743034467329de67d95d29d325e1bb191e
haolee <imleehao@gmail.com>
2019-06-10 15:51:48 +0800

    update power func

--- 剑指offer/Power.py
+++ 剑指offer/Power.py
@@ -4,9 +4,10 @@
 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
 '''
 
+'''
 def Power(base, exponent):
-    if not isinstance(base, float) and not isinstance(exponent, int):
-        return None
+    if base == 0:
+        return 0
     res = 1
     if exponent > 0:
         for i in range(exponent):
@@ -16,3 +17,21 @@ def Power(base, exponent):
         for j in range(abs(exponent)):
             res *= base
         return 1 / res
+'''
+
+
+class Solution:
+    def Power(self, base, exponent):
+        if base == 0:
+            return 0
+        res = 1
+        b = base
+        e = abs(exponent)
+        while e > 0:
+            if e & 1:
+                res *= b
+            # e // 2 to find 1 from low pos to high pos
+            e >>= 1
+            # power(b,subexponent)
+            b *= b
+        return res if exponent >= 0 else 1/res
