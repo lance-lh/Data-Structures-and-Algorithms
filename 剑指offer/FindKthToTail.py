2739bfbb186d50eb1d4a1d69fe8f87de87a9d822
haolee <imleehao@gmail.com>
2019-06-12 19:51:19 +0800

    FindKthToTail

--- 剑指offer/FindKthToTail.py
+++ 剑指offer/FindKthToTail.py
@@ -9,6 +9,7 @@
 #         self.val = x
 #         self.next = None
 
+'''
 class Solution:
     def FindKthToTail(self, head, k):
         stack = []
@@ -18,3 +19,25 @@ class Solution:
         if k > len(stack) or k < 1:
             return
         return stack.pop(-k)
+'''
+
+class Solution:
+    def FindKthToTail(self, head, k):
+        #注意三点
+        # 1. linked list为空；
+        # 2. linked list长度小于ｋ
+        # 3. k == 0
+        if not head or k == 0:
+            return
+        ahead = behind = head
+        cnt = 0
+        # ahead和behind之间相隔了k-1的距离
+        for i in range(k-1):
+            if ahead.next:
+                ahead = ahead.next
+            else:
+                return
+        while ahead.next:
+            ahead = ahead.next
+            behind = behind.next
+        return behind
