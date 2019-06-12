c2723de2752a4ddb52b08f118db3de5ee3a38337
haolee <imleehao@gmail.com>
2019-06-12 11:39:10 +0800

    update reOrderArray

--- 剑指offer/reOrderArray.py
+++ 剑指offer/reOrderArray.py
@@ -6,6 +6,9 @@
 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
 '''
 
+'''
+# time: O(n)
+# space: O(n)
 def reOrderArray(array):
     if not array:
         return []
@@ -19,7 +22,33 @@ def reOrderArray(array):
         else:
             odd_lst.append(i)
     return odd_lst + even_lst
+'''
+
+# time: O(n^2)
+# space: O(1)
+class Solution:
+    def reOrderArray(self, array):
+        if not array:
+            return []
+        for i in range(len(array)):
+            cur = array[i]
+            # if cur is 奇数
+            if not self.isEven(array[i]):
+                pre = i - 1
+                # 找pre和pre之前的偶数
+                while pre >= 0 and self.isEven(array[pre]):
+                    array[pre+1] = array[pre]
+                    pre -= 1
+                array[pre+1] = cur
+        return array
+
+    def isEven(self,n):
+        return n & 1 == 0
+
+
+
+
 
 # test
 array = [1, 4, 6, 3, 5, 8, 4,7]
-print(reOrderArray(array))
+print(Solution().reOrderArray(array))
