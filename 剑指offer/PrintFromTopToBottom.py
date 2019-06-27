2f231befdd47a45a64c3344cc1379dc5e069b2a6
haolee <imleehao@gmail.com>
2019-06-27 20:42:59 +0800

    level order traversal

--- 剑指offer/PrintFromTopToBottom.py
+++ 剑指offer/PrintFromTopToBottom.py
@@ -4,7 +4,7 @@
 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
 '''
 
-
+'''
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
@@ -33,4 +33,30 @@ class Solution:
             # nowcoder: python 2.7
             res += tmp
             cur = new
+        return res
+'''
+
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        if not root:
+            return []
+        res, cur = [], [root]
+        while cur:
+            curval, nextrow = [], []
+            for node in cur:
+                curval.append(node.val)
+                if node.left:
+                    nextrow.append(node.left)
+                if node.right:
+                    nextrow.append(node.right)
+            # return datatype is list
+            res.extend(curval)
+            cur = nextrow
         return res
