2f3ce1ab2ce445a20808c06c9d38f82c1e224e17
haolee <imleehao@gmail.com>
2019-07-13 11:25:24 +0800

    analyze max heap without built-in module

--- fundamentals/HeapSort.py
+++ fundamentals/HeapSort.py
@@ -1,5 +1,7 @@
 # max heap
+'''
 import heapq
+
 def heapsort(arr):
     if not arr:
         return
@@ -19,7 +21,7 @@ def heapsort(arr):
 if __name__ == "__main__":
     l = [1, 9, 2, 4, 7, 6, 3]
     print(heapsort(l))
-
+'''
 
 '''
 # 寻找数组中最小的K个数,最大堆
@@ -46,36 +48,53 @@ if __name__ == "__main__":
     print(list(min_k))
 '''
 
-'''
-# not verified version
+
+# 1. build a max heap
 def buildMaxHeap(arr):
     import math
     for i in range(math.floor(len(arr)/2),-1,-1):
+        # derive their left and right child
         heapify(arr,i)
 
 def heapify(arr, i):
+    # left and right are i's child
     left = 2*i+1
     right = 2*i+2
+    # record initial largest index as i
     largest = i
+
+    # compare
     if left < arrLen and arr[left] > arr[largest]:
         largest = left
     if right < arrLen and arr[right] > arr[largest]:
         largest = right
 
+    # if largest changes(not equal to i)
     if largest != i:
+        # swap largest and i
         swap(arr, i, largest)
+        # top to down heapify to rebuild a max heap
         heapify(arr, largest)
 
+# swap to adjust
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
 def heapSort(arr):
     global arrLen
     arrLen = len(arr)
+    # 1. build a max heap
     buildMaxHeap(arr)
+    # after building a max heap
     for i in range(len(arr)-1,0,-1):
+        # swap the head 0 with current i
         swap(arr,0,i)
+        # decrease arr length
         arrLen -=1
+        # rebuild max heap with array length minus 1
         heapify(arr, 0)
     return arr
-'''
+
+if __name__ == "__main__":
+    l = [1, 9, 2, 4, 7, 6, 3]
+    print(heapSort(l))
