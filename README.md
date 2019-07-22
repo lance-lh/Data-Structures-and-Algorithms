76e95fc1305ad083694cd42284b04d182eca77f0
haolee <imleehao@gmail.com>
2019-07-22 22:05:06 +0800

    rotate two strings

--- README.md
+++ README.md
@@ -244,6 +244,7 @@
   - [66. Motion Range](剑指offer/movingCount.py)
   - [Print 1 To Max N Digits](剑指offer/PrintNumber.py)
   - [1.2 Print Tree](剑指offer/printTree.py)
+  - [1.4 Rotate Two strings](剑指offer/chkRotation)
   - [8.3 Swap without extra variable](剑指offer/getSwap.py)
   - [8.4 Comparison without operation](剑指offer/getMax.py)
   - [8.5 Search odd](剑指offer/findOdd.py)
---
+++ 剑指offer/chkRotation.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+'''
+如果对于一个字符串A，将A的前面任意一部分挪到后边去形成的字符串称为A的旋转词。
+比如A="12345",A的旋转词有"12345","23451","34512","45123"和"51234"。
+对于两个字符串A和B，请判断A和B是否互为旋转词。
+
+给定两个字符串A和B及他们的长度lena，lenb，请返回一个bool值，代表他们是否互为旋转词。
+
+测试样例：
+"cdab",4,"abcd",4
+返回：true
+'''
+class Rotation:
+    def chkRotation(self, A, lena, B, lenb):
+        # write code here
+        if lena != lenb:
+            return False
+        A = A + A
+
+        # here, KMP is better since it can achieve O(m+n) time complexity
+        if B in A:
+            return True
+        else:
+            return False
