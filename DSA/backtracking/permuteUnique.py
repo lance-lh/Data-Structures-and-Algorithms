e8afe7edad20b79b9ed2e14c5d25e64d42dbffbe
lance-lh <imleehao@gmail.com>
2019-08-27 15:13:00 +0800

    permute unique

--- DSA/backtracking/permuteUnique.py
+++ DSA/backtracking/permuteUnique.py
@@ -1,20 +1,40 @@
-def permuteUnique(nums):
-    res = []
-    nums.sort()
-    dfs(nums, [], res)
-    return res
+'''
+Given a collection of numbers that might contain duplicates,
+return all possible unique permutations.
 
-def dfs(nums, path, res):
-    if not nums:
-        res.append(path)
+Example:
 
-    for i in range(len(nums)):
-        # the goal of using continue is to delete some unnecessary parts
-        # here, nums[i] == nums[i - 1] shows what sort method does
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
-        dfs(nums[:i] + nums[i + 1:], path + [nums[i]], res)
+    Input: [1,1,2]
+    Output:
+    [
+      [1,1,2],
+      [1,2,1],
+      [2,1,1]
+    ]
+'''
 
-# test
-nums = [1,1,2]
-print(permuteUnique(nums))
+
+class Solution:
+    def permuteUnique(self, nums):
+        '''
+        :param nums: List[int]
+        :return: List[List[int]]
+        '''
+        res = []
+        nums.sort()
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # jump off this loop and continue
+            self.dfs(nums[:i] + nums[i + 1:], path + [nums[i]], res)
+
+if __name__ == 'main':
+    nums = [1,1,2]
+    print(Solution().permuteUnique(nums))
+
