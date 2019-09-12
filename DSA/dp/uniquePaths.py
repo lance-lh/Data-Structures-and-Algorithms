364370e310efeec801f29536e0ae9aa3ebff5158
lance-lh <imleehao@gmail.com>
2019-09-12 14:47:51 +0800

    initialization for unique path

--- DSA/dp/uniquePaths.py
+++ DSA/dp/uniquePaths.py
@@ -34,6 +34,7 @@ class Solution(object):
         # dp represents when u arrive at (i,j) pos, how many ways need
         # all pos is initialized with 1
         # start from (1,1)
+        '''
         dp = [[1 for _ in range(m)] for _ in range(n)]
         # row n
         for i in range(1,n):
@@ -41,7 +42,15 @@ class Solution(object):
             for j in range(1,m):
                 dp[i][j] = dp[i][j-1] + dp[i-1][j]
         return dp[-1][-1]
-
+        '''
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        # add one of following code is ok.
+        # dp[0][1] = 1
+        dp[1][0] = 1
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
 # test
 m = 7
 n = 3
